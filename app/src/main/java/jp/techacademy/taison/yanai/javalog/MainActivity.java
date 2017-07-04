25f6fff54498b6ea7178daf7a83204ee3f90e085
sonyanai <taison0930@outlook.jp>
2017-07-05 06:36:10 +0900

    いろいろ追加

--- app/src/main/java/jp/techacademy/taison/yanai/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/MainActivity.java
@@ -10,6 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("javatest","ログへの出力テスト");     //ここを追加
+
+        int num = 10;
+        Log.d("javatest", String.valueOf(num));       //numの表示
+
+        num = 50;
+        Log.d("javatest", String.valueOf(num));        //javatestで引っ張られる値ですよ
+
+        int num2 = 10 + 5 - 2 * 4 / 2;
+        Log.d("javatest","10 + 5 - 2 * 4 / 2 = " + String.valueOf(num2));
+
+        boolean flag1 = true;
+        boolean flag2 = false;
+        Log.d("javatest","flag1 && flag2 = " + String.valueOf(flag1 && flag2));
+        Log.d("javatest","flag1 || flag2 = " + String.valueOf(flag1 || flag2));
+
+        int num3 = 10;
+        int num4 = 20;
+        Log.d("javatest","num3 < num4 = " + String.valueOf(num3 < num4));
+
+        Log.d("javatest","ログへの出力テスト");     //ログへの出力テストというテキストの表示
     }
 }
