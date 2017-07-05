8c54a726e53050b8ad7e5d153b9822528e8e35ef
sonyanai <taison0930@outlook.jp>
2017-07-05 23:24:33 +0900

    コメントアウトしたお

--- app/src/main/java/jp/techacademy/taison/yanai/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/MainActivity.java
@@ -11,11 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int num = 10;
+        /*int num = 10;
         Log.d("javatest", String.valueOf(num));       //numの表示
 
         num = 50;
-        Log.d("javatest", String.valueOf(num));        //javatestで引っ張られる値ですよ
+        Log.d("javatest", String.valueOf(num));        //javatestで引っ張られる値ですよ*/
 
         int num2 = 10 + 5 - 2 * 4 / 2;
         Log.d("javatest","10 + 5 - 2 * 4 / 2 = " + String.valueOf(num2));
