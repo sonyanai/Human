7ce99fe89d391216d93dc7700b0777c7fe013624
sonyanai <taison0930@outlook.jp>
2017-07-08 07:57:33 +0900

    bigdog

--- app/src/main/java/jp/techacademy/taison/yanai/javalog/BigDog.java
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/BigDog.java
@@ -4,18 +4,15 @@ package jp.techacademy.taison.yanai.javalog;
  * Created by taiso on 2017/07/08.
  */
 
+
 import android.util.Log;
+class BigDog extends Dog{
+    static String to_jp="大型犬";
 
-
-
-class BigDog extends Dog {
-    static String to_jp ="大型犬";
-
-    public BigDog(String name, int age) {
-        super(name, age);
+    public BigDog(String name,int age){
+        super(name,age);
     }
-    public static void introduce() {
-    Log.d("javatest","これは大型犬クラスです");
+    public static void introduce(){
+        Log.d("javatest","これは大型犬クラスです");
     }
 }
-
