a5371ce2f9eeb65cbbe50a782536ab235123fcfa
sonyanai <taison0930@outlook.jp>
2017-07-07 22:08:56 +0900

    dog+rewrite

--- app/src/main/java/jp/techacademy/taison/yanai/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/MainActivity.java
@@ -12,32 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int drink = 1;
-
-        switch (drink) {
-            case 0:
-                Log.d("javatest", "aaaa");
-                break;
-            case 1:
-                Log.d("javatest", "aaaa");
-                break;
-            case 2:
-                Log.d("javatest", "cccc");
-                break;
-            default:
-                Log.d("javatest", "dddd");
-                break;
-
-            Dog dog =new Dog("ポチ",3);
+            Dog dog = new Dog("ポチ", 3);
 
             dog.say();
-            Log.d("javatest", "犬の名前は" + "dog.name" + "です");
-            Log.d("javatest", "犬の年齢は" + "dog.age" + "歳です");
+            Log.d("javatest", "犬の名前は" + dog.name + "です");
+            Log.d("javatest", "犬の年齢は" + dog.age + "歳です");
 
-            Dog dog2 =new Dog("ハチ",10);
+            Dog dog2 = new Dog("ハチ", 10);
             dog2.say();
-            Log.d("javatest", "犬の名前は" + "dog2.nam歳e" + "です");
-            Log.d("javatest", "犬の年齢は" + "dog2.age" + "です");
+            Log.d("javatest", "犬の名前は" + dog2.name + "です");
+            Log.d("javatest", "犬の年齢は" + dog2.age + "歳です");
         }
     }
-}
