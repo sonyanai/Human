a20571d8dcbbfebc98bfdaf2e38a690cdc149e23
sonyanai <taison0930@outlook.jp>
2017-07-07 20:18:10 +0900

    makeclass

---
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/Dog.java
@@ -0,0 +1,21 @@
+package jp.techacademy.taison.yanai.javalog;
+
+/**
+ * Created by taiso on 2017/07/07.
+ */
+import android.util.Log;
+
+class Dog {
+
+    String name;
+    int age;
+
+    public Dog() {
+        name = "";
+        age = 0;
+    }
+    public void say(){
+        Log.d("javatest",this.name+"("+this.age+"歳)"+"「ワンワン」");
+    }
+
+}
