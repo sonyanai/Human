3e0858ddbad0e68bcbb5cb6ad289a11b4d2287dd
sonyanai <taison0930@outlook.jp>
2017-07-09 08:32:00 +0900

    kadai

--- app/src/main/java/jp/techacademy/taison/yanai/javalog/Human.java
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/Human.java
@@ -5,12 +5,19 @@ import android.util.Log;
  * Created by taiso on 2017/07/09.
  */
 
-class Human extends Animal {
+class Human extends Animal implements Thinkable {
     public Human(String name,int age) {
         this.name = name;
         this.age = age;
     }
     public void say(){
         Log.d("javatest","私の名前は"+this.name + "です。年は" +this.age+ "歳です。" );
+    }
+    @Override
+    public void think(){
+        public Thinkable(String think){
+            this.think=think;
+        }
+        Log.d("javatest","私は"+this.think+"について考える");
     }
 }
---
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/Thinkable.java
@@ -0,0 +1,10 @@
+package jp.techacademy.taison.yanai.javalog;
+
+/**
+ * Created by taiso on 2017/07/09.
+ */
+
+interface Thinkable {
+    public void think();
+}
+
