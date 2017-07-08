be7688653b85b721cb5bdd9a9d4f06a5ea9fc7d7
sonyanai <taison0930@outlook.jp>
2017-07-08 19:09:39 +0900

    abstract

---
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/Animal.java
@@ -0,0 +1,12 @@
+package jp.techacademy.taison.yanai.javalog;
+
+/**
+ * Created by taiso on 2017/07/08.
+ */
+
+abstract class Animal {
+    String name;
+    int age;
+
+    abstract public void say();
+}
