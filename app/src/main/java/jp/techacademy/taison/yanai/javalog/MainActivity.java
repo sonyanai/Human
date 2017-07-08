3f3b4c0e2b281edb9eb699c424032941e40bd9ee
sonyanai <taison0930@outlook.jp>
2017-07-08 19:18:35 +0900

    rename

--- app/src/main/java/jp/techacademy/taison/yanai/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("javatest", "犬の名前は" + dog.name + "です");
         Log.d("javatest", "犬の年齢は" + dog.age + "歳です");
 
-        BigDog bigdog = new BigDog("ヨーゼフ",15);
+        BigDog bigdog = new BigDog("太郎",15);
 
         bigdog.say();
         Log.d("javatest", "犬の名前は" + bigdog.name + "です");
