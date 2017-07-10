852696411a189cf4b24986db70c6a4f10f168d48
sonyanai <taison0930@outlook.jp>
2017-07-10 12:01:58 +0900

    kadai

--- app/src/main/java/jp/techacademy/taison/yanai/javalog/Hobby.java
+++ app/src/main/java/jp/techacademy/taison/yanai/javalog/Hobby.java
@@ -4,8 +4,10 @@ package jp.techacademy.taison.yanai.javalog;
  * Created by taiso on 2017/07/09.
  */
 
-abstract class Hoby {
-    public String hoby ="aaaa";
+abstract class Hobby {
+    public String hobby ="aaaa";
+
+
 
     abstract public void think();
 }
